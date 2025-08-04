d591a385e2ded94139cb7aeda0ccb604750de06e
zozzzC <czlc05edu@gmail.com>
2025-08-04 20:15:39 +1200

    feat: change serving frontend build files in express to only occur when in production

--- packages/backend/src/routes/routes.ts
+++ packages/backend/src/routes/routes.ts
@@ -13,10 +13,14 @@ import apiV1Routes from './api/api';
 router.use('/api/v1', apiV1Routes);
 
 // Express serve React frontend build files
-router.use(express.static(path.join(__dirname, '../../../frontend/dist')));
-router.get('*', (_, res) => {
-  res.sendFile(path.join(__dirname, '../../../frontend/dist/index.html'));
-});
+// Only enable this if we are using prod build
+
+if (process.env.NODE_ENV === 'production') {
+  router.use(express.static(path.join(__dirname, '../../../frontend/dist')));
+  router.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../../../frontend/dist/index.html'));
+  });
+}
 
 /**
  * Export the router so it can be used outside.
