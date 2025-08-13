5ac7c9bc0c2588fd31a4e96ea918897c0da9d4fe
Dahyun Ha <162376263+dha524@users.noreply.github.com>
2025-08-13 15:54:52 +1200

    added golden divider and fixed margins in both mobile and pc

--- packages/frontend/src/pages/SponsorPage.tsx
+++ packages/frontend/src/pages/SponsorPage.tsx
@@ -7,11 +7,13 @@ export const SponsorPage: React.FC = () => {
     // Layout changes depending on screen size
     <div className="min-h-screen">
       <section className="bg-black px-4 py-10 flex flex-col items-center text-center">
-        <h1 className="md:text-6xl text-3xl font-black font-[Inter] text-[#E5CE63] bg-black md:mb-20 mb-10 md:mt-50 mt-40">
+        <h1 className="md:text-6xl text-3xl font-black font-[Inter] text-[#E5CE63] bg-black md:mb-20 mb-10 md:mt-50 mt-20">
           OUR SPONSORS FOR 2025
         </h1>
       </section>
-      <section className="bg-white px-4 py-10 flex flex-col items-center text-center">
+      <section className="bg-[#E5CE63] h-5">
+      </section>
+      <section className="bg-white px-4 py-3 flex flex-col items-center md:mb-20 mb-10 md:mt-10 mt-5">
         <Sponsors />
       </section>
     </div>
