f46417f5487ce294ac0078ea0b40bd4173fc5214
Dahyun Ha <162376263+dha524@users.noreply.github.com>
2025-08-13 15:34:13 +1200

    changed background colours in the sponsor page

--- packages/frontend/src/pages/SponsorPage.tsx
+++ packages/frontend/src/pages/SponsorPage.tsx
@@ -5,11 +5,15 @@ export const SponsorPage: React.FC = () => {
   return (
     // Title layout and sponsor images and text layout on one page
     // Layout changes depending on screen size
-    <main className="min-h-screen bg-black px-4 py-10 flex flex-col items-center text-center">
-      <h1 className="md:text-6xl text-3xl font-black font-[Inter] text-[#E5CE63] md:mb-10 mb-5">
-        OUR SPONSORS FOR 2025
-      </h1>
-      <Sponsors />
-    </main>
+    <div className="min-h-screen">
+      <section className="bg-black px-4 py-10 flex flex-col items-center text-center">
+        <h1 className="md:text-6xl text-3xl font-black font-[Inter] text-[#E5CE63] bg-black md:mb-20 mb-10 md:mt-50 mt-40">
+          OUR SPONSORS FOR 2025
+        </h1>
+      </section>
+      <section className="bg-white px-4 py-10 flex flex-col items-center text-center">
+        <Sponsors />
+      </section>
+    </div>
   );
 };
