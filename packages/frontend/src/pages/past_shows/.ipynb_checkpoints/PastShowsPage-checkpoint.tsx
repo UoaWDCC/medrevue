dd87886cfeac649008e3d849d144a555b916b397
rayishihara <rayishihara06@gmail.com>
2026-05-10 14:19:46 +1200

    feat: add card in hero section displaying the show for 2026

--- packages/frontend/src/pages/past_shows/.ipynb_checkpoints/PastShowsPage-checkpoint.tsx
+++ packages/frontend/src/pages/past_shows/.ipynb_checkpoints/PastShowsPage-checkpoint.tsx
@@ -1,15 +1,27 @@
-import Card from '../../components/CardStack/Card';
-import CardStack from '../../components/CardStack/CardStack';
-import './PastShowsPage.css';
+import CardStack from '../../components/CardStack';
+import CurrentShowCard, {type CurrentShowCardProps} from '../../components/Cards/CurrentShowCard';
 
 export default function PastShowsPage() {
 
+    const currentShowInfo: CurrentShowCardProps = {
+        year: "2026",
+        title: "The Consultant of Oz",
+        posterUrl: "placeholder.png",
+        description:
+            "Auckland Medical Revue 2026 reimagines the iconic world of Oz through a bold and comedic medical lens. This year's production, The Consultant of Oz, brings together medical students from the University of Auckland in a high‑energy theatrical performance blending satire, music, and storytelling.",
+        dates: "13th – 15th August 2026",
+        time: "07:30pm – 10:00pm",
+        doors: "Doors open at 06:45pm",
+        location: "SkyCity Theatre, Corner Hobson Street and Wellesley Street West, Auckland 1010"
+    }
+
     return (
-        <>
-            <section className="bg-[#ffc6bf] w-screen h-[100vh]">
-                {/* <Card info={card} /> */}
+        <div className="overflow-x-hidden">
+            <section className="bg-[var(--colour-background-primary)] w-screen h-[100vh] flex flex-col justify-center items-center">
+                <h1 className="text-[56px] pb-5 text-[var(--colour-background-secondary)]">Our 2026 Show</h1>
+                <CurrentShowCard {...currentShowInfo}/>
             </section>
             <CardStack />
-        </>
-    )
+        </div>
+    );
 }
