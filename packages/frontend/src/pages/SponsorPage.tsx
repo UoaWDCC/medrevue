37e17ea1f3f84e02079d3370b8129e9f6aebe33b
AugensteinT <taug868@aucklanduni.ac.nz>
2026-05-13 13:37:42 +1200

    refactor(theme): remove local theme override and revert to global styling

--- packages/frontend/src/pages/SponsorPage.tsx
+++ packages/frontend/src/pages/SponsorPage.tsx
@@ -7,34 +7,17 @@ import { PreviousSponsors } from '../components/Sponsors/Sponsors.tsx';
 
 export const SponsorPage: React.FC = () => {
   return (
-    // TODO: Move CSS variables to a global stylesheet and remove the inline style tag
     <>
-      <style>{`
-        :root {
-          --font-sans: "Poppins", ui-sans-serif, system-ui, sans-serif;
-
-          --colour-theme-pink: #fe7ee5;
-          --colour-theme-green: #4cbf46;
-          --colour-text-light: #fdf7e4;
-          --colour-background-primary: #000000;
-          --colour-background-white: #ffffff;
-          --colour-background-secondary: #f2d558;
-          --colour-secondary-darker: #d8ba35;
-          --colour-text-brown: #ab751c;
-          --colour-text-grey: #313131;
-          --colour-text-light-grey: #828282;
-        }
-      `}</style>
       <div className="min-h-screen">
         {/* Hero Section */}
-        <section className="bg-[var(--colour-background-primary)] px-4 py-10 flex flex-col items-center text-center">
-          <h1 className="block text-[96px] leading-none font-semibold font-(family-name:--font-sans) text-[var(--colour-background-secondary)] mb-4 mt-8 whitespace-nowrap">
+        <section className="bg-background-primary px-4 py-10 flex flex-col items-center text-center">
+          <h1 className="block text-[96px] leading-none font-semibold font-(family-name:--font-sans) text-background-secondary mb-4 mt-8 whitespace-nowrap">
             Sponsor Us
           </h1>
-          <p className="text-[40px] italic font-[Open_Sans] text-[var(--colour-text-light)] max-w-3xl">
+          <p className="text-[40px] italic font-[Open_Sans] text-text-light max-w-3xl">
             Partner with Auckland Medical Revue
           </p>
-          <div className="bg-[var(--colour-background-white)] rounded-[46px] p-6 md:p-10 max-w-1xl my-10 mx-7">
+          <div className="bg-background-white rounded-[46px] p-6 md:p-10 max-w-1xl my-10 mx-7">
             <p className="text-black text-[24px] font-normal leading-[170%] font-(family-name:--font-sans) mb-8">
               Auckland Medical Revue is one of New Zealand’s largest student-led
               theatrical productions, created and performed by medical students
@@ -58,10 +41,10 @@ export const SponsorPage: React.FC = () => {
               className="
                 order-first
                 lg:order-none
-                bg-[var(--colour-background-secondary)]
+                bg-background-secondary
                 rounded-full w-fit px-5 py-2.5
                 text-[18px] italic font-semibold font-[Inter] leading-[36px] text-black
-                no-underline transition-colors duration-300 hover:bg-[var(--colour-secondary-darker)]
+                no-underline transition-colors duration-300 hover:bg-secondary-darker
               "
             >
               Contact Us
@@ -69,8 +52,8 @@ export const SponsorPage: React.FC = () => {
           </div>
         </section>
 
-        <section className="bg-[var(--colour-background-secondary)] px-11 py-10 grid grid-cols-1 md:grid-cols-2 gap-6 text-left">
-          <div className="bg-[var(--colour-background-secondary)] p-4 md:p-8">
+        <section className="bg-background-secondary px-11 py-10 grid grid-cols-1 md:grid-cols-2 gap-6 text-left">
+          <div className="bg-background-secondary p-4 md:p-8">
             <h2 className="block text-[40px] leading-none font-semibold font-(family-name:--font-sans) text-black mb-4">
               Your Support Creates Real Impact
             </h2>
@@ -80,7 +63,7 @@ export const SponsorPage: React.FC = () => {
             </p>
           </div>
 
-          <div className="shadow-[0_4px_8px_rgba(0,0,0,0.25)] bg-[var(--colour-background-white)] rounded-[32px] p-6 md:p-10">
+          <div className="shadow-[0_4px_8px_rgba(0,0,0,0.25)] bg-background-white rounded-[32px] p-6 md:p-10">
             <h2 className="flex items-center gap-3 text-[40px] leading-none font-semibold font-(family-name:--font-sans) text-black mb-4">
               <img
                 src={exposureIcon}
@@ -95,7 +78,7 @@ export const SponsorPage: React.FC = () => {
             </p>
           </div>
 
-          <div className="shadow-[0_4px_8px_rgba(0,0,0,0.25)] bg-[var(--colour-background-white)] rounded-[32px] p-6 md:p-10">
+          <div className="shadow-[0_4px_8px_rgba(0,0,0,0.25)] bg-background-white rounded-[32px] p-6 md:p-10">
             <h2 className="flex items-center gap-3 text-[40px] leading-none font-semibold font-(family-name:--font-sans) text-black mb-4">
               <img src={peopleIcon} alt="People" className="w-13.5 h-13.5" />
               Audience Reach
@@ -106,7 +89,7 @@ export const SponsorPage: React.FC = () => {
             </p>
           </div>
 
-          <div className="shadow-[0_4px_8px_rgba(0,0,0,0.25)] bg-[var(--colour-background-white)] rounded-[32px] p-6 md:p-10">
+          <div className="shadow-[0_4px_8px_rgba(0,0,0,0.25)] bg-background-white rounded-[32px] p-6 md:p-10">
             <h2 className="flex items-center gap-3 text-[40px] leading-none font-semibold font-(family-name:--font-sans) text-black mb-4">
               <img src={charityIcon} alt="Charity" className="w-13.5 h-13.5" />
               Charity Impact
@@ -117,55 +100,55 @@ export const SponsorPage: React.FC = () => {
           </div>
         </section>
 
-        <section className="bg-[var(--colour-background-white)] px-11 mt-10 text-center">
+        <section className="bg-background-white px-11 mt-10 text-center">
           <h2 className="block text-[40px] leading-none font-semibold font-(family-name:--font-sans) text-black mb-4">
             Our Impact
           </h2>
         </section>
-        <section className="bg-[var(--colour-background-white)] px-11 mt-10 mb-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 text-left">
-          <div className="px-4 md:px-8 border-l-4 border-[var(--colour-text-brown)]">
-            <p className="block text-[60px] font-bold leading-[124%] font-(family-name:--font-sans) text-[var(--colour-text-grey)] mb-3">
+        <section className="bg-background-white px-11 mt-10 mb-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 text-left">
+          <div className="px-4 md:px-8 border-l-4 border-text-brown">
+            <p className="block text-[60px] font-bold leading-[124%] font-(family-name:--font-sans) text-text-grey mb-3">
               148
             </p>
-            <p className="block text-[20px] font-semibold leading-[124%] font-(family-name:--font-sans) text-[var(--colour-text-light-grey)]">
+            <p className="block text-[20px] font-semibold leading-[124%] font-(family-name:--font-sans) text-text-light-grey">
               Total Cast Members
               <br />
               (Across 2 Years)
             </p>
           </div>
-          <div className="px-4 md:px-8 border-l-4 border-[var(--colour-text-brown)]">
-            <p className="block text-[60px] font-bold leading-[124%] font-(family-name:--font-sans) text-[var(--colour-text-grey)] mb-3">
+          <div className="px-4 md:px-8 border-l-4 border-text-brown">
+            <p className="block text-[60px] font-bold leading-[124%] font-(family-name:--font-sans) text-text-grey mb-3">
               3
             </p>
-            <p className="block text-[20px] font-semibold leading-[124%] font-(family-name:--font-sans) text-[var(--colour-text-light-grey)]">
+            <p className="block text-[20px] font-semibold leading-[124%] font-(family-name:--font-sans) text-text-light-grey">
               Unforgettable
               <br />
               Show Nights
             </p>
           </div>
-          <div className="px-4 md:px-8 border-l-4 border-[var(--colour-text-brown)]">
-            <p className="block text-[60px] font-bold leading-[124%] font-(family-name:--font-sans) text-[var(--colour-text-grey)] mb-3">
+          <div className="px-4 md:px-8 border-l-4 border-text-brown">
+            <p className="block text-[60px] font-bold leading-[124%] font-(family-name:--font-sans) text-text-grey mb-3">
               1864
             </p>
-            <p className="block text-[20px] font-semibold leading-[124%] font-(family-name:--font-sans) text-[var(--colour-text-light-grey)]">
+            <p className="block text-[20px] font-semibold leading-[124%] font-(family-name:--font-sans) text-text-light-grey">
               Tickets Sold
             </p>
           </div>
-          <div className="px-4 md:px-8 border-l-4 border-[var(--colour-text-brown)]">
-            <p className="block text-[60px] font-bold leading-[124%] font-(family-name:--font-sans) text-[var(--colour-text-grey)] mb-3">
+          <div className="px-4 md:px-8 border-l-4 border-text-brown">
+            <p className="block text-[60px] font-bold leading-[124%] font-(family-name:--font-sans) text-text-grey mb-3">
               $10.5k
             </p>
-            <p className="block text-[20px] font-semibold leading-[124%] font-(family-name:--font-sans) text-[var(--colour-text-light-grey)]">
+            <p className="block text-[20px] font-semibold leading-[124%] font-(family-name:--font-sans) text-text-light-grey">
               Raised for Charity
             </p>
           </div>
         </section>
 
         {/* Divider */}
-        <section className="bg-[var(--colour-background-primary)] h-30" />
+        <section className="bg-background-primary h-30" />
 
         {/* Sponsors Content */}
-        <section className="bg-[var(--colour-background-white)] px-4 py-3 flex flex-col items-center md:mb-20 mb-10 md:mt-15 mt-10">
+        <section className="bg-background-white px-4 py-3 flex flex-col items-center md:mb-20 mb-10 md:mt-15 mt-10">
           <h2 className="flex items-center gap-3 text-[40px] leading-none font-semibold font-(family-name:--font-sans) text-black">
             Previous Sponsors
           </h2>
@@ -174,7 +157,7 @@ export const SponsorPage: React.FC = () => {
         </section>
 
         {/* Bottom Divider */}
-        <section className="bg-[var(--colour-background-primary)] h-30" />
+        <section className="bg-background-primary h-30" />
       </div>
     </>
   );
