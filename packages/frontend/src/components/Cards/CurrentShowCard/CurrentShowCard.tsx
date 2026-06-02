ab92b806b76af76cccc054a8d57bffcd4d95cf27
rayishihara <rayishihara06@gmail.com>
2026-06-03 01:57:24 +1200

    fix: the gap around the poster was actally intentional

--- packages/frontend/src/components/Cards/CurrentShowCard/CurrentShowCard.tsx
+++ packages/frontend/src/components/Cards/CurrentShowCard/CurrentShowCard.tsx
@@ -95,7 +95,7 @@ export default function CurrentShowCard({
               <img
                 src={posterUrl}
                 alt={`${title} poster`}
-                className="w-full h-full object-fill scale-105"
+                className="w-full h-full object-fill"
               />
             ) : (
               <div className="w-full h-full bg-[black]" />
