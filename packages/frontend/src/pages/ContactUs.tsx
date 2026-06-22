a97fa585d3b355317eb2d191c97cd86833d45a61
Maya Babu <mayababu@Mayas-MacBook-Air-Jarvis.local>
2026-06-22 16:52:23 +1200

    feat: created object that stores information from form

--- packages/frontend/src/pages/ContactUs.tsx
+++ packages/frontend/src/pages/ContactUs.tsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+
 export const ContactPage: React.FC = () => {
+  const [form, setForm] = useState({
+    // stores the info gotten from form
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  });
+
   return (
     <div>
       <h1>test</h1>
