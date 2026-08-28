c8d7fdd13b2b949beb8adea3ab4b193480ed32d8
rayishihara <rayishihara06@gmail.com>
2026-08-28 07:21:49 +0000

    feat: wire cmsApi into the redux store

--- packages/frontend/src/redux/store.ts
+++ packages/frontend/src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { cmsApi } from '../services/cms/cmsApi';
 import seatSelectionReducer from './slices/seatSelectionSlice';
 
 export const store = configureStore({
   reducer: {
     seatSelection: seatSelectionReducer,
+    [cmsApi.reducerPath]: cmsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cmsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
