73f4d71516959cbd9e25e0db9ea55904af5abb54
rayishihara <rayishihara06@gmail.com>
2026-08-29 23:22:19 +0000

    feat: add Sponsor, Contact, and ThemeSettings adapters

---
+++ packages/frontend/src/services/cms/models/ThemeSettings.ts
@@ -0,0 +1,12 @@
+export type ThemeSettings = {
+  primaryBackground: string;
+  secondaryBrand: string;
+  secondaryHover: string;
+  lightText: string;
+  white: string;
+  primaryText: string;
+  mutedText: string;
+  accent1: string;
+  accent2: string;
+  warmBrown: string;
+};
