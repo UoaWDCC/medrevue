e6bc81ab17dd9881306034dde6e5a0b325d51fc3
rayishihara <rayishihara06@gmail.com>
2026-08-29 23:04:37 +0000

    feat: add useGetCurrentShowQuery hook

--- packages/frontend/src/services/cms/cmsApi.ts
+++ packages/frontend/src/services/cms/cmsApi.ts
@@ -30,7 +30,25 @@ export const cmsApi = createApi({
         }
       },
     }),
+    getCurrentShow: builder.query<Show | null, void>({
+      queryFn: async (_arg, api) => {
+        try {
+          const data = await cmsClient.getCollection(
+            'shows',
+            {
+              where: { isCurrentShow: { equals: true } },
+              limit: 1,
+              signal: api.signal,
+            },
+            showAdapter,
+          );
+          return { data: data.docs[0] ?? null };
+        } catch (error) {
+          return { error: error as CmsError };
+        }
+      },
+    }),
   }),
 });
 
-export const { useGetShowsQuery } = cmsApi;
+export const { useGetShowsQuery, useGetCurrentShowQuery } = cmsApi;
